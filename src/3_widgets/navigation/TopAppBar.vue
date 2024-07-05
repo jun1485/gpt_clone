0a738ea9db36b85aba272ac1620928b7aa93abf7
주정준 <65483521+jun1485@users.noreply.github.com>
2024-07-05 09:16:29 +0900

    💄[DESIGN] 상단 앱바 레이아웃 작업

--- src/3_widgets/navigation/TopAppBar.vue
+++ src/3_widgets/navigation/TopAppBar.vue
@@ -1,23 +1,46 @@
 <script setup lang="ts">
-import { MicrophoneIcon } from "@heroicons/vue/16/solid";
+import {
+  BellIcon,
+  MicrophoneIcon,
+  PlusIcon,
+  UserIcon,
+} from "@heroicons/vue/16/solid";
 </script>
 
 <template>
   <div
-    class="fixed top-0 flex justify-between items-center bg-black w-full h-14"
+    class="fixed top-0 flex justify-between items-center bg-black w-full h-14 px-4"
   >
     <div class="flex-1">JunTube</div>
-    <div class="flex-1 flex justify-center gap-3">
+    <div class="flex-1 flex justify-center gap-3 h-10">
       <input
         type="text"
         placeholder="검색"
-        class="bg-transparent focus:ring-1 focus:ring-blue-500 outline-none"
+        class="bg-white/10 focus:ring-1 focus:ring-blue-500 outline-none"
       />
-      <div class="p-1.5 bg-gray-400 rounded-full">
+      <div
+        class="flex justify-center items-center p-1.5 bg-gray-400 rounded-full w-10"
+      >
         <MicrophoneIcon class="h-5 w-5" />
       </div>
     </div>
-    <div class="flex-1">3</div>
+    <div class="flex-1 flex items-center justify-end gap-2">
+      <div
+        class="flex justify-center items-center w-10 h-10 rounded-full hover:bg-white/15"
+      >
+        <PlusIcon class="h-6 w-6" />
+      </div>
+      <div
+        class="flex justify-center items-center w-10 h-10 rounded-full hover:bg-white/15"
+      >
+        <BellIcon class="h-6 w-6" />
+      </div>
+      <div
+        class="flex justify-center items-center w-10 h-10 rounded-full hover:bg-white/15"
+      >
+        <UserIcon class="h-6 w-6" />
+      </div>
+    </div>
   </div>
 </template>
 
