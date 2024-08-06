90b8420bda3e98db3db1a985ebfa1ab76cb28669
주정준 <65483521+jun1485@users.noreply.github.com>
2024-08-06 22:12:29 +0900

    👷[CHORE] get DB 데이터 통신로직 수정

--- src/4_features/chat/api/query.ts
+++ src/4_features/chat/api/query.ts
@@ -1,7 +1,7 @@
 import { useQuery, UseQueryReturnType } from "@tanstack/vue-query";
 import { ChatType } from "../../../5_entities/chat/model/type";
 import { ComputedRef } from "vue";
-import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "@/server/firebase";
 
 // 모든 채팅 데이터를 가져오는 함수
@@ -10,20 +10,25 @@ export const getDBChats = async (): Promise<ChatType[]> => {
   const chatSnapshot = await getDocs(chatCollection);
 
   const chatList = chatSnapshot.docs.map((doc) => doc.data() as ChatType);
+
   return chatList;
 };
 
 // 특정 채팅 데이터를 가져오는 함수
 export const getDBSelectedChat = async (
-  id: number | null
+  uuid: string | null
 ): Promise<ChatType | null> => {
-  if (id === null) return null;
+  if (uuid === null) return null;
 
   const chatCollection = collection(db, "chats");
-  const chatSnapshot = await getDoc(doc(chatCollection, id.toString()));
+  const q = query(chatCollection, where("id", "==", uuid)); // UUID를 기준으로 쿼리
+  const chatSnapshot = await getDocs(q);
 
-  if (chatSnapshot.exists()) {
-    return chatSnapshot.data() as ChatType;
+  if (!chatSnapshot.empty) {
+    const chatDoc = chatSnapshot.docs[0];
+    return {
+      ...chatDoc.data(),
+    } as ChatType;
   } else {
     return null;
   }
@@ -36,13 +41,13 @@ export const useChatQuery = (): UseQueryReturnType<ChatType[], unknown> =>
   });
 
 export const useSelectedChatQuery = (
-  id: ComputedRef<number | null>
+  id: ComputedRef<string | null>
 ): UseQueryReturnType<ChatType | null, unknown> =>
   useQuery({
     queryKey: ["chat", id],
     queryFn: () => {
       if (id.value !== null) {
-        return getDBSelectedChat(id.value as number);
+        return getDBSelectedChat(id.value.toString());
       }
       return null;
     },
