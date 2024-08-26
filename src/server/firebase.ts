22940742e1b7a2018a43377852746c9878b4fc47
주정준 <65483521+jun1485@users.noreply.github.com>
2024-08-26 09:14:53 +0900

    👷[CHORE] firebase 규칙 주석처리

--- src/server/firebase.ts
+++ src/server/firebase.ts
@@ -28,18 +28,19 @@ export { db };
 /**
  * Firebase 규칙 설정
  */
-rules_version = '2';
-service cloud.firestore {
-  match /databases/{database}/documents {
-    
-    // 모든 사용자가 모든 데이터 읽기 및 쓰기 가능
-    match /{document=**} {
-      allow read, write: if true;
-    }
-    
-    // 특정 경로에 대해서만 허용
-    match /chats/{chatID} {
-      allow read, write: if request.auth != null;
-    }
-  }
-}
+
+// rules_version = '2';
+// service cloud.firestore {
+//   match /databases/{database}/documents {
+
+//     // 모든 사용자가 모든 데이터 읽기 및 쓰기 가능
+//     match /{document=**} {
+//       allow read, write: if true;
+//     }
+
+//     // 특정 경로에 대해서만 허용
+//     match /chats/{chatID} {
+//       allow read, write: if request.auth != null;
+//     }
+//   }
+// }
