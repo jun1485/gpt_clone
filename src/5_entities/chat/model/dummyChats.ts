b5517873c91160e1372e1b899747f18ebeba223e
jun <wnwjdwns1@naver.com>
2024-09-02 09:02:59 +0900

    👷[CHORE] dummy 데이터 수정

--- src/5_entities/chat/model/dummyChats.ts
+++ src/5_entities/chat/model/dummyChats.ts
@@ -2,19 +2,25 @@ import { ChatType } from "./type";
 
 export const dummyChats: ChatType[] = [
   {
-    id: 1,
+    id: "1",
     title: "Hello",
-    content: "Hello, World!",
+    messages: [
+      { id: "1", content: "Hello, World!", timestamp: "2021-09-01T12:00:00" },
+    ],
   },
   {
-    id: 2,
+    id: "2",
     title: "Goodbye",
-    content: "Goodbye, World!",
+    messages: [
+      { id: "1", content: "Goodbye, World!", timestamp: "2021-09-01T12:00:00" },
+    ],
   },
 ];
 
 export const selectedChat = {
   id: 1,
   title: "Hello",
-  content: "Hello, World!",
+  messages: [
+    { id: "1", content: "Hello, World!", timestamp: "2021-09-01T12:00:00" },
+  ],
 };
