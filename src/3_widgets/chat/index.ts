19237ed0afee6e969f172624b45c94baac26ba2d
jun <wnwjdwns1@naver.com>
2024-09-12 09:25:56 +0900

    👷[CHORE] ChatContents 폴더 이동

---
+++ src/3_widgets/chat/index.ts
@@ -0,0 +1,1 @@
+export { default as ChatContents } from "./ChatContents.vue";
--- src/3_widgets/navigation/index.ts
+++ src/3_widgets/navigation/index.ts
@@ -1,2 +1,1 @@
 export { default as SideBar } from "./SideBar.vue";
-export { default as ChatContents } from "./ChatContents.vue";
