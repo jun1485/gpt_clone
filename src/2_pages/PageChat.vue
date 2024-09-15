983d060dea2db9ab119d1b119aeade24efbfa935
jun <wnwjdwns1@naver.com>
2024-09-15 14:46:20 +0900

    👷[CHORE] PageChat에 ChatContents 컴포넌트 적용

--- src/2_pages/PageChat.vue
+++ src/2_pages/PageChat.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ChatContents } from "@/3_widgets/chat";
 import { getDBSelectedChat } from "@/4_features/chat/api/query";
 import { ChatType } from "@/5_entities/chat/model/type";
 import InputMessage from "@/6_shared/ui/InputMessage.vue";
@@ -16,27 +17,12 @@ const fetchSelectedChatData = async () => {
   }
 };
 
-const handleSendMessage = async (message: string) => {
-  console.log(message);
-};
-
 onMounted(fetchSelectedChatData);
 </script>
 
 <template>
   <div class="flex flex-col gap-3 px-6 py-4 w-full h-full justify-between">
-    <h1 class="text-xl">My GPT</h1>
-
-    <div class="flex flex-col gap-3 grow justify-end overflow-y-scroll mb-5">
-      <div v-if="selectedChatData === null" class="mx-auto">
-        <p>채팅을 선택해주세요!</p>
-      </div>
-      <div v-else v-for="message in selectedChatData.messages" class="mx-auto">
-        {{ message.content }}
-      </div>
-    </div>
-
-    <InputMessage @send="handleSendMessage" />
+    <ChatContents />
   </div>
 </template>
 
