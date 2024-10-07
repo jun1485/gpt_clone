92f1b623627def4c48108d20d0808b296447ed33
jun <wnwjdwns1@naver.com>
2024-10-07 10:03:48 +0900

    👷[CHORE] API 키값 env파일로 이동

--- src/server/firebase.ts
+++ src/server/firebase.ts
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDR7QdPQJWH7z_L96iuLAAYKZZZYpNUN4g",
-  authDomain: "gpt-clone-2d9b8.firebaseapp.com",
-  projectId: "gpt-clone-2d9b8",
-  storageBucket: "gpt-clone-2d9b8.appspot.com",
-  messagingSenderId: "648411162988",
-  appId: "1:648411162988:web:67d21be9da8e3039082b49",
-  measurementId: "G-Z2M1LTNM8K",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
