a5d68c8c4718b671b9be32e79a571127078992dd
jun <wnwjdwns1@naver.com>
2024-10-17 09:20:11 +0900

    ✨[FEAT] 새로고침 시 로그인 유지

--- src/1_app/App.vue
+++ src/1_app/App.vue
@@ -1,10 +1,34 @@
 <script setup lang="ts">
 import "@/1_app/globals.scss";
+import { useAuth } from "@/6_shared/composables/useAuth";
+import { useRouter } from "vue-router";
+import { watch } from "vue";
+
+const { isAuthenticated, isLoading } = useAuth();
+const router = useRouter();
+
+watch([isAuthenticated, isLoading], ([newIsAuthenticated, newIsLoading]) => {
+  if (!newIsLoading) {
+    if (newIsAuthenticated) {
+      if (router.currentRoute.value.path === "/") {
+        router.push("/home");
+      }
+    } else {
+      if (router.currentRoute.value.path !== "/") {
+        router.push("/");
+      }
+    }
+  }
+});
 </script>
 
 <template>
-  <div>
+  <div v-if="!isLoading">
     <router-view></router-view>
+  </div>
+  <div v-else>
+    <!-- 로딩 인디케이터 -->
+    로딩 중...
   </div>
 </template>
 
