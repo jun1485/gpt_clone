ec4576277a4e4edfb1d2dd7bea8c936a4a036713
jun <wnwjdwns1@naver.com>
2024-11-07 09:36:48 +0900

    ✨[FEAT] useResponsive 유틸함수 추가

---
+++ src/6_shared/composables/useResponsive.ts
@@ -0,0 +1,17 @@
+import { computed } from "vue";
+import { useWindowSize } from "@vueuse/core";
+
+export function useResponsive() {
+  const { width } = useWindowSize();
+
+  const isMobile = computed(() => width.value < 768);
+  const isTablet = computed(() => width.value >= 768 && width.value < 1024);
+  const isDesktop = computed(() => width.value >= 1024);
+
+  return {
+    isMobile,
+    isTablet,
+    isDesktop,
+    width,
+  };
+}
